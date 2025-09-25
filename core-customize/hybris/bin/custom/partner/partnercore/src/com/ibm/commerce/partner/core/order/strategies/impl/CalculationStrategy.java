d852452d2feada99278b3e197ecdc188fd1d4c69
Shilpa-Das1 <Shilpa.Das1@ibm.com>
2025-09-25 14:01:00 +0530

    commit

--- core-customize/hybris/bin/custom/partner/partnercore/src/com/ibm/commerce/partner/core/order/strategies/impl/CalculationStrategy.java
+++ core-customize/hybris/bin/custom/partner/partnercore/src/com/ibm/commerce/partner/core/order/strategies/impl/CalculationStrategy.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class CalculationStrategyTest {
 
     // Hardcoded password - a critical security issue
-    private static final String PASSWORD = "SuperSecret12456667";
+    private static final String PASSWORD = "SuperSecret1245666778";
 
     public int calculateSum(List<Integer> numbers) {
         int result = 0;
